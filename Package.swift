2bbd2b82959cc9ea02ad6681b762a5a8a86320bb
Daniel Too <dan@emojieggplant.com>
2024-02-08 16:01:56 +1300

    add support for visionOS

--- Package.swift
+++ Package.swift
@@ -9,6 +9,8 @@ let package = Package(
                  .macOS(.v10_15),
                  .tvOS(.v13),
                  .watchOS(.v6)
+//                 ,
+//                 .visionOS(.v1)
     ],
     products: [
         .library(
